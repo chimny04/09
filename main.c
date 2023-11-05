b47889ff8e269351f6485f33e64ae528a1575bf1
chimny04 <lucia.roback@gmail.com>
2023-11-05 12:59:48 +0900

    9-2-2

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int i, average;
+    int sum = 0;
     int grade[SIZE];
-    printf("5명의 점수를 입력하세요.\n"); 
     
-    for(i=0; i<SIZE; i++)
+    printf("input 5 scores: "); 
+    for(i=0; i<SIZE; i++) {
         scanf("%d", &grade[i]);
+        sum += grade[i];
+    }
     
-    for(i=0; i<SIZE; i++)
-        printf("grade[%d] = %d\n", i, grade[i]);
+    average = sum / SIZE;
+    
+    printf("score average = %d\n", average);
  
     system("PAUSE");	
     return 0;
